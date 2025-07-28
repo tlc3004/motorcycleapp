efbf734bfe537dd1cb1cecc73a42b0488401f366
tlc3004 <leonantoniobm@gmail.com>
2025-07-28 12:46:25 -0500

    comit

--- src/components/Timon.jsx
+++ src/components/Timon.jsx
@@ -1,19 +1,23 @@
-import React from "react";
-import './Timon.css'
+import React, { useState, useEffect } from "react";
+import './Timon.css';
 
+export default function Timon({ rotacion }) {
+  const [entrar, setEntrar] = useState(false);
 
+  useEffect(() => {
+    const timer = setTimeout(() => setEntrar(true), 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
-
-export default function Timon({rotacion}){
-
-    return(
-        <div className="timon-container"
-        style={{
-            transform:`translate(-50%, -50%) rotate(${rotacion}deg)`,
-            transition: "transform 5s ease"
-        }}
-        >
-            <img src="/images/motosbg2.png" alt="timon de moto" className="timon-img"/>
-        </div>
-    );
+  return (
+    <div
+      className={`timon-container ${entrar ? 'timon-entrar' : ''}`}
+      style={{
+        transform: `translate(-50%, -50%) rotate(${rotacion}deg)`,
+        transition: "transform 5s ease"
+      }}
+    >
+      <img src="/images/motosbg2.png" alt="timon de moto" className="timon-img" />
+    </div>
+  );
 }
